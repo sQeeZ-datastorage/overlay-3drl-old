982513cc4db14bcbbfc491d598c135903f023549
marcel-noehre <marcelnoehre@web.de>
2022-07-08 18:27:48 +0200

    series handling

--- js/contol.js
+++ js/contol.js
@@ -10,31 +10,67 @@ function toggleSeriesElement(id) {
         case 'blue-1':
             {
                 blue1 = blue1 ? false : true;
+                localStorage.setItem('blue-1', blue1);
+                if (blue1) {
+                    document.getElementById('blue1').style.backgroundColor = "blue";
+                } else {
+                    document.getElementById('blue1').style.backgroundColor = "transparent";
+                }
                 break;
             }
         case 'blue-2':
             {
                 blue2 = blue2 ? false : true;
+                localStorage.setItem('blue-2', blue2);
+                if (blue2) {
+                    document.getElementById('blue2').style.backgroundColor = "blue";
+                } else {
+                    document.getElementById('blue2').style.backgroundColor = "transparent";
+                }
                 break;
             }
         case 'blue-3':
             {
                 blue3 = blue3 ? false : true;
+                localStorage.setItem('blue-3', blue3);
+                if (blue3) {
+                    document.getElementById('blue3').style.backgroundColor = "blue";
+                } else {
+                    document.getElementById('blue3').style.backgroundColor = "transparent";
+                }
                 break;
             }
         case 'orange-1':
             {
                 orange1 = orange1 ? false : true;
+                localStorage.setItem('orange-1', orange1);
+                if (orange1) {
+                    document.getElementById('orange1').style.backgroundColor = "orange";
+                } else {
+                    document.getElementById('orange1').style.backgroundColor = "transparent";
+                }
                 break;
             }
         case 'orange-2':
             {
                 orange2 = orange2 ? false : true;
+                localStorage.setItem('orange-2', orange2);
+                if (orange2) {
+                    document.getElementById('orange2').style.backgroundColor = "orange";
+                } else {
+                    document.getElementById('orange2').style.backgroundColor = "transparent";
+                }
                 break;
             }
         case 'orange-3':
             {
                 orange3 = orange3 ? false : true;
+                localStorage.setItem('orange-3', orange3);
+                if (orange3) {
+                    document.getElementById('orange3').style.backgroundColor = "orange";
+                } else {
+                    document.getElementById('orange3').style.backgroundColor = "transparent";
+                }
                 break;
             }
         default:
---
+++ js/localstorage.js
@@ -0,0 +1,8 @@
+window.addEventListener('storage', function() {
+    document.getElementById('blue-1').style.visibility = localStorage.getItem('blue-1') == 'true' ? 'visible' : 'hidden';
+    document.getElementById('blue-2').style.visibility = localStorage.getItem('blue-2') == 'true' ? 'visible' : 'hidden';
+    document.getElementById('blue-3').style.visibility = localStorage.getItem('blue-3') == 'true' ? 'visible' : 'hidden';
+    document.getElementById('orange-1').style.visibility = localStorage.getItem('orange-1') == 'true' ? 'visible' : 'hidden';
+    document.getElementById('orange-2').style.visibility = localStorage.getItem('orange-2') == 'true' ? 'visible' : 'hidden';
+    document.getElementById('orange-3').style.visibility = localStorage.getItem('orange-3') == 'true' ? 'visible' : 'hidden';
+}, false);
