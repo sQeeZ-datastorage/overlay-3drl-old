9dfe037d5ee9b0bd1ff44bff394ea9e085f201b2
noehrem <96351170+noehrem@users.noreply.github.com>
2022-11-11 01:43:05 +0100

    hold animation for gamestats dialog

--- js/gamestats.js
+++ js/gamestats.js
@@ -6,9 +6,10 @@ function hideGameStats() {
     $("#gamestats").empty(); 
 }
 
-export function showGameStats() {
+export async function showGameStats() {
     //TODO: calculate values
     //TODO: Show Dialog
+    await new Promise(res => setTimeout(res, 4500));
     $("#gamestats").load("./../html/gamestats.html"); 
 }
 
