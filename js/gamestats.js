b1913294be5db1e40dc6fa394fb60a4804c1b8bf
noehrem <96351170+noehrem@users.noreply.github.com>
2022-12-16 09:58:33 +0100

    load gamestats into progress bars

--- js/gamestats.js
+++ js/gamestats.js
@@ -32,19 +32,38 @@ export function updateBoostConsumption(team, boost) {
 }
 
 function displayPlayerStats(players, currentPlayers) {
+    //TODO: rework progress bar fills
     $("#gamestats-left").empty();
     $("#gamestats-right").empty();
+    $("#gamestats-comparison").empty();
+    var teamStats = [['score',0,0],['goals',0,0],['assists',0,0],['saves',0,0],['shots',0,0],['demos',0,0],['ball touches',0,0]];
     players.forEach(player => {
         var container;
+        var team;
         for(var i = 0; i < currentPlayers.length; i++) {
             if(currentPlayers[i].id === player.id) {
                 container = currentPlayers[i].team === 'blue' ? $("#gamestats-left") : $("#gamestats-right");
+                team = currentPlayers[i].team === 'blue' ? 1 : 2;
             }
         }
         container.append($("<div class=\"gamestats-player\"><p>" + player.name 
         + "</p><hr><p>" + player.score + "</p><p>" + player.goals  + "</p><p>" 
         + player.assists  + "</p><p>" + player.saves  + "</p><p>" + player.shots 
         + "</p><p>" + player.demos + "</p><p>" + player.touches + "</p></div>"));
+        teamStats[0][team] += player.score;
+        teamStats[1][team] += player.goals;
+        teamStats[2][team] += player.assists;
+        teamStats[3][team] += player.saves;
+        teamStats[4][team] += player.shots;
+        teamStats[5][team] += player.demos;
+        teamStats[6][team] += player.touches;
+    });
+    teamStats.forEach(stat => {
+        var blue = (stat[1]+stat[2]) === 0 ? 1 : stat[1];
+        var sum = (stat[1]+stat[2]) === 0 ? 2 : (stat[1]+stat[2]);
+        $("#gamestats-comparison").append("<div class=\"seperator\"><div class=\"label\">" + stat[0] 
+        + "</div><progress class=\"progress\" value=\"" + blue + "\" max=\"" 
+        + sum + "\"></progress></div>");
     });
 }
 
